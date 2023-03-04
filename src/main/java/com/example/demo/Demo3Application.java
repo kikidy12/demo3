dd41df93e494d0494fdc307be66af9ad018a947b
kikidy12 <30651543+kikidy12@users.noreply.github.com>
2023-03-04 22:30:51 +0900

    Update Demo3Application.java

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo3Application {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(Demo3Application.class, args);
 	}
