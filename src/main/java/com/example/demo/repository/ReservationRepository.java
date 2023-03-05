be533c96fba9cb61b39badc791d5dc0ffc6f4413
kikidy12 <kikidy12@naver.com>
2023-03-05 11:03:19 +0900

    test123

--- src/main/java/com/example/demo/repository/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.airbnbpractice.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-}
