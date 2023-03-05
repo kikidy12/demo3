ead3322d29ffb903e40f14473aae7a176c5eddf1
kikidy12 <kikidy12@naver.com>
2023-03-05 11:23:58 +0900

    test123

--- src/test/java/com/example/demo/Demo3ApplicationTests.java
+++ src/test/java/com/example/demo/Demo3ApplicationTests.java
@@ -6,6 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Demo3ApplicationTests {
 
+
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
+
 	@Test
 	void contextLoads() {
 	}
